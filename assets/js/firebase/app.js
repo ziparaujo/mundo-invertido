3695a972771d6717127344b1f32baaa11f1e39ad
Rodrigo Araujo <ziparaujo@gmail.com>
2022-09-01 00:15:57 -0300

    integrando formulario com o firebase

---
+++ assets/js/firebase/app.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.9.3/firebase-app.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDa_FeM96YRu_neMtxNRLSlr4ff2FJvZEQ",
+  authDomain: "mundo-invertido-79af6.firebaseapp.com",
+  projectId: "mundo-invertido-79af6",
+  storageBucket: "mundo-invertido-79af6.appspot.com",
+  messagingSenderId: "774779028775",
+  appId: "1:774779028775:web:21be1095b6d35151d79502"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
